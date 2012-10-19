ee91a5477e09fb4c0ca75c43cf332d33494b768c
Nic Wolff <nic@angel.net>
2012-10-18 23:49:20 -0400

    Remove VAT and Togglegrid fields

--- lib/HTML/DataForm/Field/DBI/Togglegrid.pm
+++
@@ -1,8 +0,0 @@
-package HTML::DataForm::Field::DBI::Togglegrid;
-use base qw( 
-	HTML::DataForm::Field::DBI::MultiplySelectable 
-	HTML::DataForm::Field::DBI::Base 
-	HTML::DataForm::Field::CGI::Togglegrid 
-);
-
-1;
--- lib/HTML/DataForm/Field/DBI/Vat.pm
+++
@@ -1,4 +0,0 @@
-package HTML::DataForm::Field::DBI::Vat;
-use base qw( HTML::DataForm::Field::DBI::Base HTML::DataForm::Field::CGI::Vat );
-
-1;
