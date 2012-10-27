ed3dc20c6d006c409aca1431d183de5c0e72c275
Nic Wolff <nic@angel.net>
2012-10-26 23:05:33 -0400

    Use HTML::FromArrayref in HTML::DataForm::CGI

--- lib/HTML/DataForm/CGI.pm
+++ lib/HTML/DataForm/CGI.pm
@@ -1,53 +1,39 @@
 package HTML::DataForm::CGI;
 use base HTML::DataForm;
 
+use HTML::FromArrayref;
+
 sub print_page {
-	my ($me, $text, $bad_fields) = @_;
+	my ($me, $body, $bad_fields) = @_;
 
-	my ($bgcolor, @html, $line);
-
-	$bgcolor = 'bgcolor=' . $me->{bgcolor} if $me->{bgcolor};
+	$body ||= $me->print_html( $bad_fields );
 
 	# Print the HTML page header
 	print qq(Content-type: text/html\n\n);
 
 	if ( $me->{html_template} ) {
-		@html = split /PUT CONTENT HERE/, $me->{html_template};
-		print shift( @html );
+		my @html = split /PUT CONTENT HERE/, $me->{html_template};
+		print $html[0], $body, $html[1];
 	} elsif ( $me->{template} ) {
 		open( TEMPLATEF, $me->{template} );
-		while ( defined( $line = <TEMPLATEF> ) and $line !~ /PUT CONTENT HERE/ ) {
-			$line =~ s/<field:([^>]+)>/$me->{$1}/g;
-			print $line;
-		}
-	} else {
-		my $title = join ' / ', grep $_, ( $me->{hed}, $me->{title} );
-		print qq(<html><head><title>$title</title>);
-		print qq(<link rel=StyleSheet href="$me->{stylesheet}" type="text/css">)
-			if $me->{stylesheet};
-		print qq(<style>$me->{style}</style>) if $me->{style};
-		print qq(</head><body $bgcolor>);
-	}
-
-	# Print the form
-	if ( $text ) {
-		print $text;
-	} else {
-		$me->print_html( $bad_fields );
-	}
-
-	# Print the HTML page footer
-	if ( $me->{html_template} ) {
-		print shift( @html );
-	} elsif ( $me->{template} ) {
-		while ( $line = <TEMPLATEF> ) {
+		while ( defined( my $line = <TEMPLATEF> ) ) {
+			print $body if $line =~ /PUT CONTENT HERE/;
 			$line =~ s/<field:([^>]+)>/$me->{$1}/g;
 			print $line;
 		}
 		close TEMPLATEF;
 	} else {
-		print q(</body></html>);
+		my $title = join ' / ', grep $_, $me->{hed}, $me->{title};
+		print HTML [ html =>
+			[ head =>
+				[ title => $title || 'Form' ],
+				$me->{stylesheet} && [ link => { rel => 'StyleSheet', href => $me->{stylesheet}, type => 'text/css' } ],
+				$me->{style} && [ style => $me->{style} ]
+			],
+			[ body => $body ]
+		];
 	}
+
 }
 
 1;
