ca0275d9e6e54e60ecdd625b6563631cb61b65dc
Nic Wolff <nic@angel.net>
2012-10-26 23:24:50 -0400

    Don't use base Field::DBI::Date from itself

--- lib/HTML/DataForm/Field/DBI/Date.pm
+++ lib/HTML/DataForm/Field/DBI/Date.pm
@@ -1,7 +1,6 @@
 package HTML::DataForm::Field::DBI::Date;
-use base qw( 
-	HTML::DataForm::Field::DBI::Date 
-	HTML::DataForm::Field::CGI::Date 
+use base qw(
+	HTML::DataForm::Field::CGI::Date
 	HTML::DataForm::Field::DBI::Datetime
 );
 
